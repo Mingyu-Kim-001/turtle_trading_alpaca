cf2671b6ec4e4172e0a5bd152ae3d800c11211d2
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2025-11-02 14:21:47 -0800

    add init

---
+++ system/__init__.py
@@ -0,0 +1,9 @@
+"""
+Turtle Trading System Package
+
+A systematic trend-following trading system based on the Turtle Trading rules.
+"""
+
+from .turtle_trading import TurtleTrading
+
+__all__ = ['TurtleTrading']
